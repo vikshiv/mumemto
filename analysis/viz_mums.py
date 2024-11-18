b6e6f41bd04d336a4963fb5a522a7bc65b30fca9
Vikram Shivakumar <vshivak1@jhu.edu>
2024-11-18 18:20:41 -0500

    added collinear blocking for fast viz

--- analysis/viz_mums.py
+++ analysis/viz_mums.py
@@ -3,6 +3,7 @@ from matplotlib.collections import PolyCollection
 import os
 import argparse
 from tqdm.auto import tqdm
+import numpy as np
 
 def parse_arguments():    
     parser = argparse.ArgumentParser(description="Plots a synteny plot of MUMs from mumemto")
@@ -20,12 +21,14 @@ def parse_arguments():
     parser.add_argument('--subsample','-s', dest='subsample', help='subsample every Nth mum', default=1, type=int)
     parser.add_argument('--center','-c', dest='center', action='store_true', help='center plot', default=False)
     parser.add_argument('--inversion-color','-ic', dest='inv_color', help='color for inversions', default='green')
-    parser.add_argument('--mum-color','-mc', dest='mum_color', help='opacity of mums [0-1]', default=0.2, type=float)
+    parser.add_argument('--mum-color','-mc', dest='mum_color', help='color as hex (default: #00A2FF)', default='#00A2FF', type=str)
+    parser.add_argument('--alpha','-a', dest='alpha', help='opacity of mums [0-1] (default: 0.5)', default=0.5, type=float)
     parser.add_argument('--fout','-o', dest='filename', help='plot fname (default: input_prefix)')
-    parser.add_argument('--dims', dest='size', help='fig dimensions (inches) (default: 20,10)', default=(20,10), type=float, nargs=2)
+    parser.add_argument('--dims', dest='size', help='fig dimensions (inches) (default: 6.4, 4.8)', default=(6.4,4.8), type=float, nargs=2)
     parser.add_argument('--dpi','-d', dest='dpi', help='dpi', default=500, type=int)
     parser.add_argument('--verbose','-v', dest='verbose', help='verbose mode', action='store_true', default=False)
-    
+    parser.add_argument('--no-coll-block','-b', dest='no_coll_block', help='plot only MUMs, not collinear blocks (slower) (default: false)', action='store_true', default=False)
+    parser.add_argument('--max-gap-len','-g', dest='max_break', help='maximum break between collinear mums within a collinear block (default: <1px)', default=None, type=int)
     args = parser.parse_args()
     if args.mumfile:
         args.prefix = os.path.splitext(args.mumfile)[0]
@@ -33,25 +36,64 @@ def parse_arguments():
     else:
         args.mumfile = args.prefix + '.mums'
         args.lens = args.prefix + '.lengths'
-    args.mum_color = 1 - args.mum_color
     if not args.filename:
         args.filename = args.prefix
     return args
 
-def get_polygons(args, mums, genome_lengths, lenfilter=0, offset=0, color=(0.8, 0.8, 0.8), inv_color='green'):
-    def points_to_poly(points):
-        starts, ends = tuple(zip(*points))
-        points = starts + ends[::-1]
-        return points
-    max_length = max(genome_lengths)
-    centering = [0] * len(genome_lengths)
-    if args.center:
-        centering = [((max_length - g) / 2) + offset for g in genome_lengths]
+def find_coll_blocks(args, mums, max_length):
+    if args.max_break == None:
+        bp_per_inch = max_length / (args.dpi * args.size[0])
+        max_break = min(bp_per_inch, 100000)
+    else:
+        max_break = args.max_break
+    if args.verbose:
+        print('max gap within a collinear block:', max_break)
+    starts = np.array([m[1] for m in mums])
+    mum_orders = starts.transpose().argsort()
+    mum_gaps = []
+    flips = set([])
+    for i in range(mum_orders.shape[0]):
+        cur = []
+        for l in range(1, mum_orders.shape[1]):
+            left, right = mum_orders[i][l-1], mum_orders[i][l]
+            if mums[left][2][i] == mums[right][2][i]:
+                if mums[left][2][i] == '+':
+                    cur.append((left, right))
+                else:
+                    cur.append((right, left))
+                    flips.add((right, left))
+        mum_gaps.append(cur)
+    common_gaps = set.intersection(*map(set, mum_gaps))
+    left, right = zip(*common_gaps)
+    left, right = set(list(left)), set(list(right))
+    true_collinear_mums = sorted(list(left.intersection(right)))
+    right_coll_mums = sorted(list(left.difference(set(true_collinear_mums)))) # have a right pair, but not a left
+    left_coll_mums = sorted(list(right.difference(set(true_collinear_mums)))) # have a left pair, but not a right
+    large_blocks = list(zip(right_coll_mums, left_coll_mums))
+    ### find the longest stretches of collinear mums
+    small_blocks = []
+    for l, r in large_blocks:
+        last = l
+        for i in range(l, r):
+            lens = np.full(len(mums[i][1]), mums[i][0])
+            lens[(mums[i+1][1] < mums[i][1])] = mums[i+1][0] 
+            gap_lens = np.abs(mums[i][1] - mums[i+1][1]) - lens
+            if gap_lens.max() > max_break and last < i:
+                small_blocks.append((last, i))
+                last = i + 1
+        if last != r:
+            small_blocks.append((last, r))
+    return large_blocks, small_blocks, mum_gaps
+
+def points_to_poly(points):
+    starts, ends = tuple(zip(*points))
+    points = starts + ends[::-1]
+    return points
+    
+def get_mum_polygons(mums, centering, color='#00A2FF', inv_color='red'):
     polygons = []
     colors = []    
     for (l, starts, strands) in mums:
-        if l < lenfilter:
-            continue
         inverted = strands[0] == '-'
         points = []
         for idx, (x, strand) in enumerate(zip(starts, strands)):
@@ -60,7 +102,7 @@ def get_polygons(args, mums, genome_lengths, lenfilter=0, offset=0, color=(0.8, 
                     polygons.append(points_to_poly(points[:-1]))
                     colors.append(color)
                 continue
-            points.append(((centering[idx] + x, idx), (centering[idx] + x + l, idx)) if strand == '+' else ((centering[idx] + genome_lengths[idx] - x - l, idx), (centering[idx] + genome_lengths[idx] - x, idx)))
+            points.append(((centering[idx] + x, idx), (centering[idx] + x + l, idx)))
             if not inverted and strand == '-':
                 inverted = True
                 if len(points) > 2:
@@ -82,17 +124,45 @@ def get_polygons(args, mums, genome_lengths, lenfilter=0, offset=0, color=(0.8, 
             colors.append(color)
     return polygons, colors
 
-def draw_synteny(genome_lengths, mums, lenfilter=0, dpi=500, size=None, genomes=None, filename=None, verbose=False):
+def get_block_polygons(collinear_blocks, mums, centering, color='#00A2FF', inv_color='red'):
+    polygons = []
+    colors = []
+    for _, (l, r) in enumerate(collinear_blocks):
+        strands = mums[l][2]
+        inverted = strands[0] == '-'
+        points = []
+        left, right = mums[l][1], mums[r][1] + mums[r][0]
+        for idx, strand in enumerate(strands):
+            points.append(((centering[idx] + left[idx], idx), (centering[idx] + right[idx], idx)))
+            if not inverted and strand == '-':
+                inverted = True
+                if len(points) > 2:
+                    polygons.append(points_to_poly(points[:-1]))
+                    colors.append(color)
+                polygons.append(points_to_poly(points[-2:]))
+                colors.append(inv_color)
+                points = [points[-1]]
+            elif inverted and strand == '+':
+                inverted = False
+                if len(points) > 2:
+                    polygons.append(points_to_poly(points[:-1]))
+                    colors.append(color)
+                polygons.append(points_to_poly(points[-2:]))
+                colors.append(inv_color)
+                points = [points[-1]]
+        if len(points) >= 2:
+            polygons.append(points_to_poly(points))
+            colors.append(color)
+    return polygons, colors
+
+def plot(genome_lengths, polygons, colors, centering, alpha=0.5, dpi=500, size=None, genomes=None, filename=None, verbose=False):
     fig, ax = plt.subplots()
     max_length = max(genome_lengths)
-    centering = [0] * len(genome_lengths)
-    if args.center:
-        centering = [(max_length - g) / 2 for g in genome_lengths]
     for idx, g in enumerate(genome_lengths):
         ax.plot([centering[idx] + 0,centering[idx] + g], [idx, idx], alpha=0.2, linewidth=0.75)
-    mums = tqdm(mums) if verbose else mums
-    polygons, colors = get_polygons(args, mums, genome_lengths, lenfilter=lenfilter, color=tuple([args.mum_color]*3), inv_color=args.inv_color)
-    ax.add_collection(PolyCollection(polygons, linewidths=0.1, alpha=0.05, edgecolors=colors, facecolors=colors))
+        
+    ax.add_collection(PolyCollection(polygons, linewidths=0, alpha=alpha, edgecolors=colors, facecolors=colors))
+    
     ax.yaxis.set_ticks(range(len(genome_lengths)))
     ax.tick_params(axis='y', which='both',length=0)
     if genomes:
@@ -107,12 +177,12 @@ def draw_synteny(genome_lengths, mums, lenfilter=0, dpi=500, size=None, genomes=
     fig.set_tight_layout(True)
     # ax.axis('off')
     
-    ax.get_yaxis().set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-    ax.grid(False)
+    # ax.get_yaxis().set_visible(False)
+    # ax.spines['top'].set_visible(False)
+    # ax.spines['right'].set_visible(False)
+    # ax.spines['left'].set_visible(False)
+    # ax.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+    # ax.grid(False)
 
     if size:
         fig.set_size_inches(*size)
@@ -131,15 +201,37 @@ def main(args):
         genome_names = [os.path.splitext(os.path.basename(l.split()[0]))[0] for l in open(args.filelist, 'r').read().splitlines()]
     else:
         genome_names = None
-    mums = parse_mums(args)
-    draw_synteny(seq_lengths, mums, lenfilter=args.lenfilter, genomes=genome_names, filename=args.filename, dpi=args.dpi, size=args.size, verbose=args.verbose)
-
-def parse_mums(args):
+    max_length = max(seq_lengths)
+    mums = list(parse_mums(args, seq_lengths))
+    mums = sorted(mums, key=lambda x: x[1][0])
+    if args.verbose:
+        print('parsed MUMs')
+    centering = [0] * len(seq_lengths)
+    if args.center:
+        centering = [(max_length - g) / 2 for g in seq_lengths]
+    if args.no_coll_block:
+        mums = [m for m in mums if (m[1] == -1).sum() == 0] # can only merge full MUMs
+        polygons, colors = get_mum_polygons(mums, centering, color=args.mum_color, inv_color=args.inv_color)
+    else:
+        _, collinear_blocks, _ = find_coll_blocks(args, mums, max_length)
+        if args.verbose:
+            print('\t-found %d collinear blocks'%(len(collinear_blocks)))
+        polygons, colors = get_block_polygons(collinear_blocks, mums, centering, color=args.mum_color, inv_color=args.inv_color)
+    if args.verbose:
+        print('built synteny plot. rendering...')
+    plot(seq_lengths, polygons, colors, centering, genomes=genome_names, alpha=args.alpha, filename=args.filename, dpi=args.dpi, size=args.size, verbose=args.verbose)
+    if args.verbose:
+        print('done.')
+def parse_mums(args, seq_lengths):
+    def reverse_strand(l, starts, strands):
+        new_starts = np.array([p if s == '+' or s == '' else seq_lengths[idx] - p - l for idx, (p, s) in enumerate(zip(starts, strands))])
+        return (l, new_starts, strands)
     count = 0
     for l in open(args.mumfile, 'r').readlines():
         if count % args.subsample == 0:
             l = l.strip().split()
-            yield int(l[0]), tuple([int(v) if v else None for v in l[1].split(',')]), tuple(l[2].split(','))
+            if int(l[0]) >= args.lenfilter:
+                yield reverse_strand(int(l[0]), [int(v) if v else -1 for v in l[1].split(',')], tuple(l[2].split(',')))
         count += 1
 if __name__ == "__main__":
     args = parse_arguments()
