dfac3319f6718c3b1f751bf68ae41052b5c339af
Vikram Shivakumar <vshivak1@jhu.edu>
2025-01-20 23:49:15 -0500

    fixed incorrect neg strand handling for mems

--- include/mem_finder.hpp
+++ include/mem_finder.hpp
@@ -149,7 +149,7 @@ protected:
             curpos = sa_buffer.at(i - buffer_start) - doc_offsets[curdoc];
             if (revcomp && curpos >= doc_lens[curdoc]) {
                 curstrand = '-';
-                curpos = curpos - doc_lens[curdoc];
+                curpos = doc_lens[curdoc] + doc_lens[curdoc] - curpos - length;
             }
             else
                 curstrand = '+';
