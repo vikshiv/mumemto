7cc969123e262672a03c8dc0a492163e14fdf6fe
Vikram Shivakumar <vshivak1@jhu.edu>
2025-01-29 16:28:11 -0500

    removed unecessary argument for -p

--- src/pfp_mum.cpp
+++ src/pfp_mum.cpp
@@ -317,7 +317,7 @@ void parse_build_options(int argc, char** argv, BuildOptions* opts) {
     int c = 0;
     int long_index = 0;
     
-    while ((c = getopt_long(argc, argv, "hi:F:o:w:sl:ra:AKk:p:m:f:", long_options, &long_index)) >= 0) {
+    while ((c = getopt_long(argc, argv, "hi:F:o:w:sl:ra:AKk:pm:f:", long_options, &long_index)) >= 0) {
         switch(c) {
             case 'h': mumemto_usage(); std::exit(0);
             case 'i': opts->input_list.assign(optarg); break;
