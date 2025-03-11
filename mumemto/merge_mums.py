2010b0c820e504a06f00379fd5b0c20534bf0291
Vikram Shivakumar <vshivak1@jhu.edu>
2025-03-11 16:31:24 -0400

    merge multiple mum sets simultaenously

--- mumemto/merge_mums.py
+++ mumemto/merge_mums.py
@@ -1,9 +1,9 @@
 import numpy as np
 import argparse
 try:
-    from utils import get_sequence_lengths
+    from utils import get_sequence_lengths, parse_mums_generator
 except ImportError:
-    from mumemto.utils import get_sequence_lengths
+    from mumemto.utils import get_sequence_lengths, parse_mums_generator
 
 
 def remove_start_dollar(mums, s1_bv):
@@ -18,74 +18,61 @@ def remove_start_dollar(mums, s1_bv):
         for idx in range(len(dollars)):
             new_l = dollars[idx] - last_pos_left
             if new_l >= 20:
-                new_starts = [s + last_pos_left if strand == '+' else s + last_pos_right - new_l for s, strand in zip(starts, strands)]
+                new_starts = [s + last_pos_left if strand else s + last_pos_right - new_l for s, strand in zip(starts, strands)]
                 new_mums.append((new_l, new_starts, strands))
             last_pos_left = dollars[idx] + 1
             last_pos_right = last_pos_right - new_l - 1
         new_l = l - last_pos_left
         if new_l >= 20:
-            new_starts = [s + last_pos_left if strand == '+' else s for s, strand in zip(starts, strands)]
+            new_starts = [s + last_pos_left if strand else s for s, strand in zip(starts, strands)]
             new_mums.append((new_l, new_starts, strands))
     return new_mums
-    
+
 def main():
     parser = argparse.ArgumentParser(description='Merge MUMs files')
-    parser.add_argument('--mums_file', '-m', required=True, help='Path to merged MUMs file')
-    parser.add_argument('--s1_mums', '-s1', required=True, help='Path to s1 MUMs file')
-    parser.add_argument('--s2_mums', '-s2', required=True, help='Path to s2 MUMs file')
+    parser.add_argument('--merged_mums', '-m', required=True, help='Path to MUMs of MUMs file')
+    parser.add_argument('mum_files', metavar='MUM_FILES', nargs='+', help='Paths to MUMs files to merge')
     parser.add_argument('--output', '-o', help='Path to output merged MUMs file', default='merged.mums')
     
     args = parser.parse_args()
     
-    if not args.mums_file.endswith('.mums'):
-        args.mums_file += '.mums'
-    if not args.s1_mums.endswith('.mums'):
-        args.s1_mums += '.mums'
-    if not args.s2_mums.endswith('.mums'):
-        args.s2_mums += '.mums'
+    assert len(args.mum_files) >= 2, "At least two MUMs files are required for merging"
+    
+    if not args.merged_mums.endswith('.mums'):
+        args.merged_mums += '.mums'
+    for i in range(len(args.mum_files)):
+        if not args.mum_files[i].endswith('.mums'):
+            args.mum_files[i] += '.mums'
 
-    with open(args.mums_file, 'r') as f:
-        mums = [line.split() for line in f.read().splitlines()]
-        mums = [[int(l[0]), [int(i) for i in l[1].split(',')], l[2].split(',')] for l in mums]
-
-    with open(args.s1_mums, 'r') as f:
-        s1 = [line.split() for line in f.read().splitlines()]
-        s1 = [[int(l[0]), [int(i) for i in l[1].split(',')], l[2].split(',')] for l in s1]
-    with open(args.s2_mums, 'r') as f:
-        s2 = [line.split() for line in f.read().splitlines()]
-        s2 = [[int(l[0]), [int(i) for i in l[1].split(',')], l[2].split(',')] for l in s2]
-        
+    premerge_mums = [list(parse_mums_generator(m)) for m in args.mum_files]
+    
     ### get lengths
-    s1_lens, s2_lens = get_sequence_lengths(args.mums_file.replace('.mums', '.lengths'), multilengths=True)
+    mum_lens = get_sequence_lengths(args.merged_mums.replace('.mums', '.lengths'), multilengths=True)
 
-    ### build bitvectors for mum starts in s1 and s2
-    s1_starts = np.cumsum(s1_lens)
-    s2_starts = np.cumsum(s2_lens)
-    s1_bv = np.zeros(sum(s1_lens) + 1, dtype=bool)
-    s1_bv[s1_starts - 1] = 1
-    s2_bv = np.zeros(sum(s2_lens) + 1, dtype=bool)
-    s2_bv[s2_starts - 1] = 1
-    breaks = [s1_starts, s2_starts]
-    offsets = [np.concatenate(([0], s1_starts)), np.concatenate(([0], s2_starts))]
+    ### build bitvectors for mum starts in comb mums
+    mum_starts = [np.cumsum(lens) for lens in mum_lens]
+    set1_bv = np.zeros(sum(mum_lens[0]) + 1, dtype=bool)
+    set1_bv[mum_starts[0] - 1] = 1
+    mum_offsets = [np.concatenate(([0], starts)) for starts in mum_starts]
 
     ### get thresholds
-    with open(args.s1_mums.replace('.mums', '.thresh'), 'rb') as f:
-        s1_thresholds = np.fromfile(f, dtype=np.uint16)
-    with open(args.s1_mums.replace('.mums', '.thresh_rev'), 'rb') as f:
-        s1_thresholds_rev = np.fromfile(f, dtype=np.uint16)
-    with open(args.s2_mums.replace('.mums', '.thresh'), 'rb') as f:
-        s2_thresholds = np.fromfile(f, dtype=np.uint16)
-    with open(args.s2_mums.replace('.mums', '.thresh_rev'), 'rb') as f:
-        s2_thresholds_rev = np.fromfile(f, dtype=np.uint16)
-        
+    thresholds, rev_thresholds = [], []
+    for m in args.mum_files:
+        with open(m.replace('.mums', '.thresh'), 'rb') as f:
+            thresholds.append(np.fromfile(f, dtype=np.uint16))
+        with open(m.replace('.mums', '.thresh_rev'), 'rb') as f:
+            rev_thresholds.append(np.fromfile(f, dtype=np.uint16))
+    
+    NUM_SETS = len(mum_lens)
+    
     ### split grandMUMs that span multiple $ in the concatenated mums into match segments
     dollar_less = []
-    for m in mums:
-        dollar_less.extend(remove_start_dollar(m, s1_bv))
+    for m in parse_mums_generator(args.merged_mums):
+        dollar_less.extend(remove_start_dollar(m, set1_bv))
 
     ### find which mum a segment belongs to both subsets
     starts = np.array([m[1] for m in dollar_less])
-    mum_idx = np.array([np.searchsorted(breaks[idx], starts[:,idx], side='right') for idx in range(2)]).transpose()
+    mum_idx = np.array([np.searchsorted(mum_starts[idx], starts[:,idx], side='right') for idx in range(NUM_SETS)]).transpose()
 
     ### main merging algorithm
     new_thresholds = []
@@ -93,63 +80,52 @@ def main():
     merged = []
     for idx, (l, starts, strands) in enumerate(dollar_less):
         # first check if it is no longer unique
-        mum_id1 = mum_idx[idx, 0]
-        # get (left offset to start of match in mum, right offset to end of match in mum)
-        offset1 = (starts[0] - offsets[0][mum_id1], offsets[0][mum_id1+1] - starts[0] - l - 1)
-        # check that the match is still unique in s1
-        thresh1 = s1_thresholds[starts[0]]
-        if thresh1 == 0 or l <= thresh1:
+        offset = []
+        thresh_check = True
+        for i in range(NUM_SETS):
+            mumid = mum_idx[idx, i]
+            # get (left offset to start of match in mum, right offset to end of match in mum)
+            offset.append((starts[i] - mum_offsets[i][mumid], mum_offsets[i][mumid+1] - starts[i] - l - 1))
+            # check that the match is still unique in subset
+            thresh = thresholds[i][starts[i]]
+            if thresh == 0 or l <= thresh:
+                thresh_check = False
+                break
+        if not thresh_check:
             continue
-        mum_id2 = mum_idx[idx, 1]
-        # get (left offset to start of match in mum, right offset to end of match in mum)
-        offset2 = (starts[1] - offsets[1][mum_id2], offsets[1][mum_id2 +1] - starts[1] - l - 1)
-        # check that the match is still unique in s2
-        thresh2 = s2_thresholds[starts[1]]
-        if thresh2 == 0 or l <= thresh2:
-            continue        
         
-        ### do 1 first, then 2
         new_starts = []
         new_strands = []
-        if strands[0] == '+': # technically always the case, but to be thorough
-            for s, strand in zip(s1[mum_id1][1], s1[mum_id1][2]):
-                new_starts.append(s + offset1[0] if strand == '+' else s + offset1[1])
-                new_strands.append(strand)
-        # else:
-        #     for s, strand in zip(s1[mum_id1][1], s1[mum_id1][2]):
-        #         new_starts.append(s + offset1 if strand == '-' else s + lendiff1)
-        #         new_strands.append('-' if strand == '+' else '+')
-        if strands[1] == '+':
-            for s, strand in zip(s2[mum_id2][1], s2[mum_id2][2]):
-                new_starts.append(s + offset2[0] if strand == '+' else s + offset2[1])
-                new_strands.append(strand)
-        else:
-            for s, strand in zip(s2[mum_id2][1], s2[mum_id2][2]):
-                new_starts.append(s + offset2[0] if strand == '+' else s + offset2[1])
-                new_strands.append('-' if strand == '+' else '+')
+        for i in range(NUM_SETS):
+            mumid = mum_idx[idx, i]
+            for s, strand in zip(premerge_mums[i][mumid][1], premerge_mums[i][mumid][2]): # get matching mum
+                new_starts.append(s + offset[i][0] if strand else s + offset[i][1])
+                # the mum in set i matches in the forward direction
+                new_strands.append(strand if strands[i] else not strand)
         merged.append((int(l), tuple([int(x) for x in new_starts]), tuple(new_strands)))
-        
-        curs1_thresh = (s1_thresholds[starts[0] : starts[0] + l], s1_thresholds_rev[offsets[0][mum_id1] + offset1[1]: offsets[0][mum_id1+1] - 1 - offset1[0]])
-        curs2_thresh = (s2_thresholds[starts[1] : starts[1] + l], s2_thresholds_rev[offsets[1][mum_id2] + offset2[1]: offsets[1][mum_id2+1] - 1 - offset2[0]])
-        if strands[1] == '-':    
-            new_thresholds.extend(np.where((curs1_thresh[0] > 0) & (curs2_thresh[1] > 0), np.maximum(curs1_thresh[0], curs2_thresh[1]), np.zeros_like(curs1_thresh[0])))
-            new_thresholds.extend([0])
-            new_thresholds_rev.extend(np.where((curs1_thresh[1] > 0) & (curs2_thresh[0] > 0), np.maximum(curs1_thresh[1], curs2_thresh[0]), np.zeros_like(curs1_thresh[1])))
-            new_thresholds_rev.extend([0])
-        else:
-            new_thresholds.extend(np.where((curs1_thresh[0] > 0) & (curs2_thresh[0] > 0), np.maximum(curs1_thresh[0], curs2_thresh[0]), np.zeros_like(curs1_thresh[0])))
-            new_thresholds.extend([0])
-            new_thresholds_rev.extend(np.where((curs1_thresh[1] > 0) & (curs2_thresh[1] > 0), np.maximum(curs1_thresh[1], curs2_thresh[1]), np.zeros_like(curs1_thresh[1])))
-            new_thresholds_rev.extend([0])
-
     
+        cur_thresh = []
+        cur_revthresh = []
+        for i in range(NUM_SETS):
+            thresh = (thresholds[i][starts[i] : starts[i] + l], rev_thresholds[i][mum_offsets[i][mum_idx[idx, i]] + offset[i][1]: mum_offsets[i][mum_idx[idx, i]+1] - 1 - offset[i][0]])
+            if strands[i]:
+                cur_thresh.append(thresh[0]); cur_revthresh.append(thresh[1])
+            else:
+                cur_revthresh.append(thresh[0]); cur_thresh.append(thresh[1])
+        cur_thresh = np.array(cur_thresh)
+        cur_revthresh = np.array(cur_revthresh)
+        new_thresholds.extend(np.where(np.all(cur_thresh > 0, axis=0), np.max(cur_thresh, axis=0), 0))
+        new_thresholds.extend([0])
+        new_thresholds_rev.extend(np.where(np.all(cur_revthresh > 0, axis=0), np.max(cur_revthresh, axis=0), 0))
+        new_thresholds_rev.extend([0])
+        
     ### write output
     # if args.output:
     if not args.output.endswith('.mums'):
         args.output += '.mums'
     with open(args.output, 'w') as f:
         for m in merged:
-            f.write('%d\t%s\t%s\n' % (m[0], ','.join(map(str, m[1])), ','.join(m[2])))
+            f.write('%d\t%s\t%s\n' % (m[0], ','.join(map(str, m[1])), ','.join(['+' if x else '-' for x in m[2]])))
     with open(args.output.replace('.mums', '.thresh'), 'wb') as f:
         f.write(np.array(new_thresholds, dtype=np.uint16).tobytes())
     with open(args.output.replace('.mums', '.thresh_rev'), 'wb') as f:
@@ -162,10 +138,9 @@ def main():
     #         sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
     #         sys.exit(0)
     with open(args.output.replace('.mums', '.lengths'), 'w') as out:
-        with open(args.s1_mums.replace('.mums', '.lengths'), 'r') as f:
-            out.write(f.read().strip() + '\n')
-        with open(args.s2_mums.replace('.mums', '.lengths'), 'r') as f:
-            out.write(f.read().strip() + '\n')
+        for m in args.mum_files:
+            with open(m.replace('.mums', '.lengths'), 'r') as f:
+                out.write(f.read().strip() + '\n')
     
 if __name__ == "__main__":
     main()
